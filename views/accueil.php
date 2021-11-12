5835f55e3860999818f24046aa86055edd666197
olivierchoquet <42567743+olivierchoquet@users.noreply.github.com>
2021-11-12 09:04:02 +0100

    Update accueil.php

--- views/accueil.php
+++ views/accueil.php
@@ -1,5 +1,6 @@
 		<section id="contenu">
 			<h2>Accueil</h2>
 			<p>Bienvenue aux séances d'exercices consacrées à élaborer un site Internet en PHP selon une architecture didactique MVC OO.</p>
+			<p> Bonjour Heroku </p>
 			<p>Jean-Luc Collinet.</p>
 		</section>
